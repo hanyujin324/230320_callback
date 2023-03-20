576680210c96b5d152cfb3b884b51562d868514a
hanyujin <hanyujin32432@gmail.com>
2023-03-20 17:18:15 +0900

    모듈

---
+++ index.js
@@ -0,0 +1,9 @@
+// const fromModuleObjectBasicData=require('./module/basic-data.js');
+//굳이 모듈을 안봐도 되어서 이름을 길게 써도 괜찮다 
+// console.dir(data);
+
+//자바스크립트는 모듈이 외장이다
+
+//작성법이 다름
+import fromModuleObjectBasicData from './module/basic-data.js';
+console.dir(fromModuleObjectBasicData);
